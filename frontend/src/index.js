8bdc47bf12ded95309bc61377f68baceccb40110
Euler Menezes Junior <euler.jr@gmail.com>
2018-11-27 18:40:54 -0200

    feat: add react-router and redux libraries and utilites

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,5 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+import middleware from './middlewares';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, middleware)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
